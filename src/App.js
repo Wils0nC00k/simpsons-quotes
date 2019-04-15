04ef96ba6a53faa40ef455f31ce10e39ebd95fca
Sarah Wilsoncook <sarahlouise125@gmail.com>
2019-04-15 09:44:15 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Lamp from './Lamp';
+import Quotes from "./Quotes";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Simpsons Quotes</h1>
+          </header>
+          <Lamp on />
+          <Lamp />
+          <Quotes />
+          </div>
     );
   }
 }
